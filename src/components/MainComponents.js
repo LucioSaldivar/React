1ca7d557236b37a0481f31c22b630ac8e2f302d6
Lucio Saldivar <manuelsaldivar310@gmail.com>
2020-07-07 15:48:46 -0500

    update

--- src/components/MainComponents.js
+++ src/components/MainComponents.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Switch, Route, Redirect, withRouter } from 'react-router-dom'
+import { Switch, Route, Redirect, withRouter } from 'react-router-dom';
 import { connect } from 'react-redux';
 import Directory from './DirectoryComponents';
 import CampsiteInfo from './CampsiteInfoComponent';
